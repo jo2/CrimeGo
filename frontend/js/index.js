eb70472cb1a4864698cddfa33b326de467e4ae20
Johannes Teklote <johannes.teklote@web.de>
2016-10-16 11:23:12 +0200

    start page list is loaded automatically

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,2 +1,11 @@
 //require("./material.js");
 //require("./ripples.js");
+const STORYURL = 'http://10.81.238.148:3000/stories';
+
+$(document).ready(function getStories() {
+    $.getJSON(STORYURL, function( data ) {
+        for (var j = 0; j < data.stories.length; j++) {
+            $('#storyList').append("\<li id='" + data.stories[j].id + "'\>" + data.stories[j].title + "\</li>");
+        }
+    });
+});
