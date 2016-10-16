ec1f193c566c9b46105165947a01afe2a4f471c9
Johannes Teklote <johannes.teklote@web.de>
2016-10-16 10:05:46 +0200

    story3 expanded

--- frontend/js/story3.js
+++ frontend/js/story3.js
@@ -8,17 +8,34 @@ var positions = [
     {
         name: "Bahnhof",
         hint: 'Am Bahnhof Schönebeck wurde eine Leiche gefunden. Da Sie zu den führenden Privatermittlern in der Region gehören, werden Sie vom zuständigen Komissar gebeten, mitzukommen.' +
-        'Das Opfer ist eine junge Frau und nach ersten Untersuchungen des Gerichtsmediziners wurde sie vorraussichtlich gegen Mitternacht erdrosselt.',
+        'Das Opfer ist eine junge Frau und nach ersten Untersuchungen des Gerichtsmediziners wurde sie vorraussichtlich gegen Mitternacht erdrosselt. Die Handtasche der Frau liegt neben der Leiche. ' +
+        'Im Portmonaie finden Sie etwas Geld, sowie den Personalausweis der Frau. Sie können jetzt entweder die Anwohner befragen oder sich bei den Angehörigen der Frau umhören.',
         pos: {
             lat: 53.17286839853227,
             lng: 8.648514747619629
         }
     }, {
-        name: "",
+        name: "Anwohner",
+        hint: 'Eine Anwohnerin berichtet, dass sie kurz vor Mitternacht zwei Personen am Bahnhof gesehen hat. Sie kann die eine Person als das Opfer identifizieren. Die andere Person ' +
+        'war ein junger Mann, groß, schlank und mit langen Haaren. Die Beiden schienen sich zu umarmen. Als dann der Zug einfuhr, sei sie wieder ins Wohnzimmer gegangen und hätte sich nichts weiter dabei gedacht. ' +
+        'Sie beschließen, zum Bahnhof zurück zu gehen und den Lokführer zu befragen.',
+        pos: {
+            lat: 53.17296486182438,
+            lng: 8.649212121963501
+        }
+    }, {
+        name: "Bahnhof",
+        hint: 'Als sie grade am Bahnhof ankommen fährt auch der Zug ein. Der Lokführer hat grade Pause und somit Zeit, sich mit Ihnen zu unterhalten. ',
+        pos: {
+            lat: 53.17286839853227,
+            lng: 8.648514747619629
+        }
+    }, {
+        name: "Angehörige",
         hint: '',
         pos: {
-            lat: 53.168417989527406,
-            lng: 8.65000605583191
+            lat: 53.173183511150214,
+            lng: 8.644019365310669
         }
     }
 ];
