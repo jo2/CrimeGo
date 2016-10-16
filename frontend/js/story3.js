446bbad1dc6f281f2d80ca0a05df5083dd1880a1
Johannes Teklote <johannes.teklote@web.de>
2016-10-16 09:44:49 +0200

    another story in german

---
+++ frontend/js/story3.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Johannes Teklote on 16.10.2016.
+ */
+/**
+ * Created by Johannes Teklote on 16.10.2016.
+ */
+var positions = [
+    {
+        name: "Bahnhof",
+        hint: 'Am Bahnhof Schönebeck wurde eine Leiche gefunden. Da Sie zu den führenden Privatermittlern in der Region gehören, werden Sie vom zuständigen Komissar gebeten, mitzukommen.' +
+        'Das Opfer ist eine junge Frau und nach ersten Untersuchungen des Gerichtsmediziners wurde sie vorraussichtlich gegen Mitternacht erdrosselt.',
+        pos: {
+            lat: 53.17286839853227,
+            lng: 8.648514747619629
+        }
+    }, {
+        name: "",
+        hint: '',
+        pos: {
+            lat: 53.168417989527406,
+            lng: 8.65000605583191
+        }
+    }
+];
