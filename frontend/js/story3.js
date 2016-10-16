94e00e2fc1db990406ca0825442fd7b5eac6ad74
Johannes Teklote <johannes.teklote@web.de>
2016-10-16 09:25:11 +0200

    another story started

---
+++ frontend/js/story3.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Johannes Teklote on 16.10.2016.
+ */
+var positions = [
+    {
+        name: "Bahnhof",
+        hint: 'A Corpse has been found at Schönebeck train station. As one of the leading local private investigators you are accompanying inspector Inspektor to the crime scene. ' +
+        'The victim is a rather young woman and a first analysis of the court physician says that she died around midnight. ',
+        pos: {
+            lat: 53.17286839853227,
+            lng: 8.648514747619629
+        }
+    }, {
+        name: "",
+        hint: '',
+        pos: {
+            lat: 53.168417989527406,
+            lng: 8.65000605583191
+        }
+    }
+];
