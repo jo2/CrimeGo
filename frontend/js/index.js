a0c13d5b61b0e4f66b6ff11173094c74877e1024
Koelli91 <kevinkoellmann@outlook.com>
2016-10-16 11:42:15 +0200

    index-list clickable

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,7 +5,16 @@ const STORYURL = 'http://10.81.238.148:3000/stories';
 $(document).ready(function getStories() {
     $.getJSON(STORYURL, function( data ) {
         for (var j = 0; j < data.stories.length; j++) {
-            $('#storyList').append("\<li id='" + data.stories[j].id + "'\>" + data.stories[j].title + "\</li>");
+            $('#storyList').append(`
+                <li class="btn">
+                    <a href="map.html?storyId=${data.stories[j].id}">
+                    <div class="list-content col-xs-10">
+                        <p>${data.stories[j].title}</p>
+                    </div>
+                    <i class="material-icons arrow-list-icon col-xs-2 text-right">keyboard_arrow_right</i>
+                    </a>
+                </li>
+            `);
         }
     });
 });
